60fc1c76a82e169f1e3f66a934568c059fd94c5b
Gabriel <gabriel.duarte18@outlook.es>
2024-05-15 22:24:26 -0400

    Handling Events

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,33 @@ const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new 
 
 export default function App() {
   const step = 1;
+
+  function handledPrevious() {
+    alert('Previous');
+  }
+
+  function handledNext() {
+    alert('Next');
+  }
+
   return (
     <div className='steps'>
       <div className='numbers'>
         <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
       </div>
 
       <p className='message'>
         Step {step}: {messages[step - 1]}
       </p>
       <div className='buttons'>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Previous</button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>Next</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handledPrevious}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handledNext}>
+          Next
+        </button>
       </div>
     </div>
   );
